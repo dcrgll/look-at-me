f13a3f2f4912ea8e2ea2a110fe71fbba9bd4660a
dan <8881965+dcrgll@users.noreply.github.com>
2022-03-10 22:45:43 +0000

    🚀Remove unused deps
    🚀Remove unused tests
    
    
    🚀 Remove unused snapsnot

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,9 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, test } from '@jest/globals'
+import { render } from '@testing-library/react'
+import { describe, test } from '@jest/globals'
 import Home from '@/pages/index'
 
 describe('Home', () => {
-  test('renders a form', () => {
+  test('renders without crashing', () => {
     render(<Home />)
-
-    const emailInput = screen.getByRole('textbox', { name: /Email/i })
-    const rememberMeInput = screen.getByRole('checkbox', { name: /Remember Me/i })
-    const submitButton = screen.getByRole('button', { name: /Sign Up/i })
-
-    expect(emailInput).toBeInTheDocument()
-    expect(rememberMeInput).toBeInTheDocument()
-    expect(submitButton).toBeInTheDocument()
   })
 })
