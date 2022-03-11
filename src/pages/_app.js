76ad924b5c216b1dae2b3757ab2a9ea8f8be4f0f
dan <8881965+dcrgll@users.noreply.github.com>
2022-03-11 19:57:05 +0000

    🚀Add SplitBee analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 import '@/styles/tailwind.css'
 import '@/styles/extra.css'
+import { useAnalytics } from '@/utils/analytics'
 
 function App ({ Component, pageProps }) {
+  useAnalytics()
   return <Component {...pageProps} />
 }
 
