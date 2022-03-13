dc4c6a54c5fb26e87bd842233a65855b1b7b557e
Dan <8881965+dcrgll@users.noreply.github.com>
2022-03-13 14:40:38 +0000

    Fix analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import "@/styles/tailwind.css";
 import "@/styles/extra.css";
-import { useAnalytics } from "@/utils/analytics";
+import { useAnalytics } from "@/config/analytics";
 
 function App({ Component, pageProps }) {
   useAnalytics();
